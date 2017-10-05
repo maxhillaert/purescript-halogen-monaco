01b9c0c7e9e81300fe7409cad10a0788e2ed088c
max <maxhillaert@users.noreply.github.com>
2017-10-05 21:03:22 +0100

    Initial Checkin Dummy button

---
+++ script/serve.js
@@ -0,0 +1,24 @@
+var express = require("express"),
+http = require("http");
+
+var runServer = function(taskName, serverPort, subdirectory) {
+if (taskName === undefined) {
+    taskName = "serve";
+}
+if (serverPort === undefined) {
+    serverPort = 5051;
+}
+if (subdirectory === undefined) {
+    subdirectory = "public";
+}
+var app = express();
+app.use(express.static(__dirname + '/../' + subdirectory));
+app.use("/lib", express.static(__dirname + '/../bower_components'));
+
+
+
+app.listen(serverPort);
+console.log('Started listening to port: ' + serverPort)
+};
+
+runServer();
