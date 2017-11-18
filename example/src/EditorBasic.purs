652da8988699158e223293566c607058c867f1ef
max <maxhillaert@users.noreply.github.com>
2017-11-18 10:36:53 +0000

    Upgrade to latest halogen/aff

--- example/src/EditorBasic.purs
+++ example/src/EditorBasic.purs
@@ -7,12 +7,16 @@ import Halogen.Aff as HA
 import Halogen.Monaco as B
 import Halogen.VDom.Driver (runUI)
 import Monaco.Types (MONACO, defaultConstuctorOptions)
-
+import Data.Maybe
 
 main :: Eff (HA.HalogenEffects (monaco :: MONACO)) Unit
 main = HA.runHalogenAff do
   body <- HA.awaitBody
   io <- runUI B.editor unit body
   let opts = defaultConstuctorOptions 
+                { value = Just "function x() {\n\tconsole.log(\"Hello world!\");\n}" 
+                    , language = Just "javascript"
+                    , theme = Just "vs-dark"
+                }
   r <- io.query (B.Init opts unit)
   pure r
