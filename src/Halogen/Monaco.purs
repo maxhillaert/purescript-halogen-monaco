9f8cd4c16551593cb71e1ec9c7f36d14dcb5fab5
nganhkhoa <mail.nganhkhoa@gmail.com>
2020-03-14 10:44:21 +0700

    Update to latest purescript library
    - Add spago
    - Effect and Aff namespace changed
    - Halogen 5.0 support, using `handleQuery`

--- src/Halogen/Monaco.purs
+++ src/Halogen/Monaco.purs
@@ -1,72 +1,74 @@
-module Halogen.Monaco where
+module Halogen.Monaco
+  ( editor
+  , EditorQuery(..)
+  , EditorMessage(..)
+  ) where
 
 import Prelude
+import Data.Int (toNumber)
+import Data.Foldable (for_)
+import Data.Maybe (Maybe(Nothing, Just))
+
+import Effect.Aff.Class (class MonadAff)
 
 import CSS.Geometry (width, height)
-import CSS.Size
-import Control.Monad.Aff (Aff)
-import Control.Monad.Eff.Exception (EXCEPTION)
-import DOM (DOM)
-import Data.Int
-import Data.Foldable (for_)
-import Data.Maybe (Maybe(Nothing, Just), maybe)
+import CSS.Size (px)
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.CSS (style)
 import Halogen.HTML.Properties as HP
+
 import Monaco.Editor as ME
-import Monaco.Types (EditorConstructionOptions, MONACO)
+import Monaco.Types (EditorConstructionOptions)
 import Monaco.Types as MT
 
 type EditorState =
   { editor ∷ Maybe MT.Editor
-   , width ∷ Maybe Int
-   , height ∷ Maybe Int
+  , width ∷ Maybe Int
+  , height ∷ Maybe Int
   }
 
-type MonacoEffects eff = (monaco :: MONACO, exception :: EXCEPTION, dom :: DOM | eff)
+data EditorAction
 
-data EditorQuery a = 
-    Init EditorConstructionOptions a
+data EditorQuery a
+  = Init EditorConstructionOptions a
 
-data EditorMessage = Initialized 
+data EditorMessage
+  = Initialized 
 
-editor :: forall eff. H.Component HH.HTML EditorQuery Unit EditorMessage (Aff (MonacoEffects eff))
+editor :: forall i m. MonadAff m => H.Component HH.HTML EditorQuery i EditorMessage m
 editor =
-  H.component
-    { initialState: const initialState
+  H.mkComponent
+    { initialState
     , render
-    , eval
-    , receiver: const Nothing
+    , eval: H.mkEval $ H.defaultEval
+      { handleQuery = handleQuery
+      }
     }
-  where
 
-  initialState :: EditorState
-  initialState = {
-      editor: Nothing,
-      width: Nothing,
-      height: Nothing
+initialState :: forall i. i -> EditorState
+initialState _ =
+  { editor: Nothing
+  , width: Nothing
+  , height: Just 1000
   }
 
-  render :: EditorState -> H.ComponentHTML EditorQuery
-  render state =
-    HH.div
-            [ HP.ref $ H.RefLabel "monacoEditor",
-              HP.class_ $ H.ClassName "monaco-editor-container",
-              style do 
-                      for_ state.height (\x->height $ px $ toNumber x)
-                      for_ state.width (\x->width $ px $ toNumber x)
-            ]
-            [  ]
+render :: forall m. EditorState -> H.ComponentHTML EditorAction () m
+render state =
+  HH.div
+    [ HP.ref $ H.RefLabel "monacoEditor"
+    , HP.class_ $ H.ClassName "monaco-editor-container"
+    , style do 
+        for_ state.height (\x -> height $ px $ toNumber x)
+        for_ state.width (\x -> width $ px $ toNumber x)
+    ]
+    [  ]
 
-  eval :: EditorQuery ~> H.ComponentDSL EditorState EditorQuery EditorMessage (Aff (MonacoEffects eff))
-  eval (Init options next) = do 
-    el' <- H.getHTMLElementRef (H.RefLabel "monacoEditor")
-    H.modify (\s -> s{ editor = Nothing})
-    for_ el' \el -> do
-        e <- H.liftAff (ME.create options el)
-        H.modify (\s -> s{editor=Just e})
-    pure next
-
-
-  
+handleQuery ∷ forall m a. MonadAff m => EditorQuery a → H.HalogenM EditorState EditorAction () EditorMessage m (Maybe a)
+handleQuery (Init options next) = do
+  el' <- H.getHTMLElementRef (H.RefLabel "monacoEditor")
+  H.modify_ (_ { editor = Nothing })
+  for_ el' \el -> do
+    e <- H.liftAff (ME.create options el)
+    H.modify_ (_ { editor = Just e })
+  pure (Just next)
